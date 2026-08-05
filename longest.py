b782db8244e2a997bb98883ae7b587ee724d9582
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-05 23:43:14 +0530

    longest substring with sum

---
+++ longest.py
@@ -0,0 +1,13 @@
+x = list(map(int, input("Enter: ").split()))
+k = int(input("Enter: "))
+def longest(x,k):
+    left = 0
+    s = 0
+    m = 0
+    for right in range(len(x)):
+        s += x[right]
+        while s > k:
+            s -= x[left]
+            left += 1
+        m = max(m, right-left+1)
+    return m
