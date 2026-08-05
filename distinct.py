327eca09bc3e3054f56ac47943ea45463d95df26
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-06 02:07:14 +0530

    atmost k distinct characters

--- distinct.py
+++ distinct.py
@@ -1,9 +1,9 @@
 x = input("Enter: ")
 k = int(input("Enter: "))
 def dis(x,k):
-    d = {}
     left = 0
     ans = 0
+    d = {}
     for right in range(len(x)):
         if x[right] not in d:
             d[x[right]] = 1
@@ -13,7 +13,7 @@ def dis(x,k):
             d[x[left]] -= 1
             if d[x[left]] == 0:
                 del d[x[left]]
-                left += 1
+            left += 1
         ans = max(ans, right-left+1)
     return ans
 print(dis(x,k))
