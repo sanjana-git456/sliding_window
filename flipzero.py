2fefd31b9dab235b63019bf95850e0fc641971b8
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-06 22:54:38 +0530

    flip zeros

---
+++ flipzero.py
@@ -0,0 +1,18 @@
+x = list(map(int, input("Enter: ").split()))
+k = int(input("Enter: "))
+def flip(x,j):
+    left = 0
+    right = 0
+    c = 0
+    ans = 0
+    while right < len(x):
+        if x[right] == 0:
+            c += 1
+        right += 1
+        while c > k:
+            if x[left] == 0:
+                c -= 1
+                left += 1
+        ans = max(ans, right-left+1)
+    return ans
+print(flip(x,k))
