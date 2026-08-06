2cd6692afaf6e881c139b51ef95ec6ee872ea563
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-07 03:05:47 +0530

    non repeating char substring

---
+++ NonRep.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class NonRep {
+
+    public int non(char[] x) {
+        int left = 0;
+        int window = x[0];
+        HashSet<Character> map = new HashSet<>();
+        for (int right = 0; right < x.length; right++) {
+            char c = x[right];
+
+        }
+    }
+}
