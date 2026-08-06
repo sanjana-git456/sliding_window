45365d56979e7e9155cdb5b757d79df5e64ad8a3
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-06 23:55:00 +0530

    max sum java

---
+++ MaxSum.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class MaxSum {
+
+    public int msum(int[] x, int k) {
+        int left = 0;
+        int right = k - 1;
+        int s = 0;
+        int ans = 0;
+        for (int i = 0; i < k - 1; i++) {
+            s += x[i];
+        }
+        while (right < x.length) {
+            right += 1;
+            s = s - x[left] + x[right];
+            left += 1;
+        }
+        ans = Math.max(ans, s);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        MaxSum ms = new MaxSum();
+        int[] arr = {2, 1, 5, 1, 3, 2};
+        System.out.println(ms.msum(arr));
+    }
+}
